75478e636fe1705a743e0012106c741e574a2385
himerushka <himerushka7@gmail.com>
2021-04-07 14:25:47 +0300

    Добавьте файлы проекта.

---
+++ Shnorr_open_keys/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shnorr_open_keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
